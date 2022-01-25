4a24575def46a1156bab549666d644d668f674d7
Vadim Hatsura <vadim.hatsura@gmail.com>
2022-01-25 23:25:00 +0100

    add analyzing of ownership topics

--- src/OrgAnalyzer/CodeOwnershipAnalyzer.cs
+++ src/OrgAnalyzer/CodeOwnershipAnalyzer.cs
@@ -1,4 +1,6 @@
 ﻿using Octokit;
+using Octokit.GraphQL;
+using Octokit.GraphQL.Core;
 
 namespace OrgAnalyzer;
 
@@ -13,9 +15,31 @@ public static class CodeOwnershipAnalyzer
 
         if (repositories.Count == 100) throw new InvalidOperationException();
 
+        var missedOwnershipTopics = new List<Repository>();
         foreach (var repository in repositories)
         {
-            
+            var query = new Query()
+                .Repository(new Arg<string>(repository.Name), new Arg<string>(repository.Owner.Login))
+                .RepositoryTopics(50)
+                .Select(repoTopics => new
+                {
+                    repoTopics.PageInfo.HasNextPage,
+                    Topics = repoTopics.Edges.Select(e => e.Node.Topic.Name).ToList()
+                })
+                .Compile();
+
+            var result = await gqlConnection.Run(query);
+
+            if (result.HasNextPage) throw new InvalidOperationException();
+
+            var ownershipTopic = result.Topics.FirstOrDefault(x => x.StartsWith("ownership-"));
+            if (ownershipTopic == null)
+            {
+                missedOwnershipTopics.Add(repository);
+                continue;
+            }
+
+            var ownershipArea = ownershipTopic.Substring(10);
         }
     }
 }
